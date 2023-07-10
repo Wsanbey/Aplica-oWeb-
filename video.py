569e2b5f491df5d0a66d34e6c7dbfdbce3c99761
Wsanbey <61056410+Wsanbey@users.noreply.github.com>
2023-07-10 15:54:14 -0300

    Atualizando programa com add files

--- video.py
+++ video.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title('Video exemplo')
+st.selectbox('Selecione uma Opção',['Opção 1', 'Opção 2'])
+
+st.sidebar.title('Menu')
+paginaSelecionada = st.sidebar.selectbox('Selecione a página',['Opção 1', 'Opção 2'])
+
+if paginaSelecionada == 'Página 1':
+    st.title('Video exemplo')
+    st.selectbox('Selecione uma Opção',['Opção 1', 'Opção 2'])
+elif paginaSelecionada == 'Página 2':
+    st.title('Video exemplo')
