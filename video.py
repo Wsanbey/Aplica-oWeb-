14396e08dbe8cdd1fc17c08f9988e8dd601ae6b7
Wsanbey <61056410+Wsanbey@users.noreply.github.com>
2023-07-10 15:59:20 -0300

    Mudançãs validas

--- video.py
+++ video.py
@@ -6,8 +6,9 @@ st.selectbox('Selecione uma Opção',['Opção 1', 'Opção 2'])
 st.sidebar.title('Menu')
 paginaSelecionada = st.sidebar.selectbox('Selecione a página',['Opção 1', 'Opção 2'])
 
-if paginaSelecionada == 'Página 1':
+if paginaSelecionada == 'Opção 1':
     st.title('Video exemplo')
     st.selectbox('Selecione uma Opção',['Opção 1', 'Opção 2'])
+
 elif paginaSelecionada == 'Opção 2':
     st.title('Você esta na pagina 2')
